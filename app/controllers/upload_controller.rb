f4b2f093dc8a3a1ef9786529ec2bee74900b5bd7
Kevin Chiu <kevin@kevinchiu.org>
2010-04-21 10:05:06 -0400

    uploader

---
+++ app/controllers/upload_controller.rb
@@ -0,0 +1,11 @@
+class UploadController < ApplicationController
+  require 'aws-s3'
+  def post
+    if(attachment = request.body.read)
+      # upload it to S3
+      
+      render :text => "s3 url"
+    else
+      render :text => "failed"
+  end
+end
