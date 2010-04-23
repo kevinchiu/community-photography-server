9b11f901822369a045f382b6a7eeb767f315e62a
Kevin Chiu <kevin@kevinchiu.org>
2010-04-23 16:03:33 -0400

    seconds

--- app/controllers/countdown_controller.rb
+++ app/controllers/countdown_controller.rb
@@ -2,6 +2,7 @@ class CountdownController < ApplicationController
   def index
     now = Time.now.to_i 
     nextMinute = 60 - now % 60
-    render :text => now + nextMinute
+    # render :text => now + nextMinute
+    render :text => now % 60
   end
 end
