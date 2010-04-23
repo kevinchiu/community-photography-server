8e22c381ef42f6b2db9db20704df42153a98d5f3
Kevin Chiu <kevin@kevinchiu.org>
2010-04-22 20:05:51 -0400

    update message

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -2,4 +2,7 @@ class UpdateController < ApplicationController
   def current
     render :text => 1
   end
+  def message
+    render :text => "Please update this app in iTunes or the App Store."
+  end
 end
