ac88e53ad52f6a212f6623e89071a111efdcc1e1
Kevin Chiu <kevin@kevinchiu.org>
2010-04-27 17:46:54 -0400

    syncing with time server pool

--- app/controllers/countdown_controller.rb
+++ app/controllers/countdown_controller.rb
@@ -1,8 +1,21 @@
+require NET
+
 class CountdownController < ApplicationController
   def index
-    now = Time.now.to_i 
-    nextMinute = 60 - now % 60
-    # render :text => now + nextMinute
-    render :text => now % 60
+    server = get_server
+    message = NTP.get_ntp_response()
+    ref = message["Reference Timestamp"]
+    ref = ref.floor
+    render :text => 60 - (ref % 60)
+  end
+  
+  protected
+  
+  def get_server
+    ["time-a.nist.gov",
+    "time-b.nist.gov",
+    "wwv.nist.gov",
+    "time.nist.gov",
+    "time-nw.nist.gov"].sort_by{rand}[0]
   end
 end
